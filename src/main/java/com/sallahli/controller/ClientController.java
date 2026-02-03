d2a138b9e314514ede03971b39fd828f8115dffa
Mohamdi <mohamedsidibrahim11@gmail.com>
2026-02-03 19:01:20 +0000

    Change the client endpoint

--- src/main/java/com/sallahli/controller/ClientController.java
+++ src/main/java/com/sallahli/controller/ClientController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/clients")
+@RequestMapping("/client")
 @RequiredArgsConstructor
 @Slf4j
 @Tag(name = "Client Management", description = "APIs for managing clients")
